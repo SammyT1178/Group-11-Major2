76816729d3985d91a0a733fd0b4c7e0507e9ef85
SammyT1178 <61430214+SammyT1178@users.noreply.github.com>
2022-12-04 21:07:24 -0600

    Update signalHandle.c

--- signalHandle.c
+++ signalHandle.c
@@ -2,5 +2,24 @@
 
 void signalHandle(int sig)
 {
-
+printf("\nWe are inside the sig function\n");
+	
+	/*if(kill(tcgetpgrp(getpid()), SIGINT) != 0)
+		perror("kill error");*/
+	
+	printf("\t\tChild PGID: %d\n", getpgid(cpid));
+	printf("\t\tParent PGID: %d\n", getpgid(ppid));
+	
+	if(getpgid(cpid) == getpgid(ppid))
+	{
+		write(1,"error\n",7);
+		return;
+	}
+	else
+	{
+		kill(cpid, SIGINT);
+		tcsetpgrp(0,ppid);
+		kill(ppid, SIGCONT);
+		printf("Parent should be working now\n");
+	}
 }
