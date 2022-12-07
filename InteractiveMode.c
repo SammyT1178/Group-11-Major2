baa9eb608eb4ca3ec3ab4af1dea33baecbdae2cc
SammyT1178 <61430214+SammyT1178@users.noreply.github.com>
2022-12-07 08:48:13 -0600

    Update InteractiveMode.c
    Added missing comments

--- InteractiveMode.c
+++ InteractiveMode.c
@@ -2,46 +2,50 @@
 
 void InteractiveMode()
 {
- int status = 0;
+ 	int status = 0;
   
-  prompt = (char*)malloc(MAX);
-  printf("Enter prompt: ");
-  fgets(prompt, MAX, stdin);
+ 	 // Prepare prompt in memory, set prompt value from user
+ 	 prompt = (char*)malloc(MAX);
+ 	 printf("Enter prompt: ");
+ 	 fgets(prompt, MAX, stdin);
   
-  //remove newline from prompt
-  if (prompt[strlen(prompt)-1] == '\n') 
-  {
-      prompt[strlen(prompt)-1] = '\0'; 
-  }
+ 	 //remove newline from prompt
+ 	 if (prompt[strlen(prompt)-1] == '\n') 
+ 	 {
+ 	     prompt[strlen(prompt)-1] = '\0'; 
+ 	 }
   
-  while(1)
-  {
-    char *str = (char*)malloc(MAX);
-    printf("%s> ", prompt);
-    fgets(str, MAX, stdin);
-    
-    if(strlen(str) == 1)
-    {
-      continue;
-    }
+ 	 while(1)
+ 	 {
+  		char *str = (char*)malloc(MAX);
+ 		printf("%s> ", prompt);
+    		fgets(str, MAX, stdin);
+    	
+    		if(strlen(str) == 1)
+    		{
+    		  continue;
+    		}
       
+    		// Remove newline
+    		if (str[strlen(str)-1] == '\n') 
+    		{
+    		  str[strlen(str)-1] = '\0';    
+    		}
     
-    if (str[strlen(str)-1] == '\n') 
-    {
-      str[strlen(str)-1] = '\0';    
-    }
-    
-    int numCmd = parseCom(str);
-    
-    for(int i=0; i < numCmd; i++){
+    		// parse commands
+    		int numCmd = parseCom(str);
+    		for(int i=0; i < numCmd; i++)
+		{
 			char *temp = strdup(command[i]);
 			temp = strtok(temp, " ");
 			exeCom(temp, command[i]);
+				
 			//free temp
 			free(temp);
 		}
-    
-    free(str);
-    
-  }
-}
+    		
+		// Free str
+		free(str);
+    	
+  	}	
+}	
