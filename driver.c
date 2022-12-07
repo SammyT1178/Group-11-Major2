8692c59d80766a91f706c7e32bd55da296920cb3
SammyT1178 <61430214+SammyT1178@users.noreply.github.com>
2022-12-07 08:56:59 -0600

    Update driver.c
    Added comments and improved readability

--- driver.c
+++ driver.c
@@ -2,22 +2,22 @@
 
 int main(int argc, char *argv[])
 {
-  if(!(argc < 3)) // if too many parameters
-  {
-    printf("Error: Too many parameters\n"); //Error message
-    exit(0);  // Exit program
-  }
+  	if(!(argc < 3)) // if too many parameters
+  	{
+  	  printf("Error: Too many parameters\n"); //Error message
+  	  exit(0);  // Exit program
+  	}
   
-  ppid = getpid(); //Set parent pid
+  	ppid = getpid(); //Set parent pid
   
-  //Signal Handles
-  signal(SIGINT, signalHandle);
-  signal(SIGTSTP, signalHandle);
+  	//Signal Handles
+  	signal(SIGINT, signalHandle);
+  	signal(SIGTSTP, signalHandle);
   
-  if(argc == 1)
-    InteractiveMode();
-  else
-    BatchMode(argv[1]);
+  	if(argc == 1) 	// If no file, jump to Interactive Mode
+  	  InteractiveMode();
+  	else		// If files, jump to Batch Mode
+  	  BatchMode(argv[1]);
   
-  return 0;
+  	return 0;
 }
