243d16b93c70b18356452758aac355e29bc2cd17
SammyT1178 <61430214+SammyT1178@users.noreply.github.com>
2022-12-07 09:00:57 -0600

    Update exeCom.c
    Added comments and improved readability

--- exeCom.c
+++ exeCom.c
@@ -2,40 +2,43 @@
 
 void exeCom(char *command, char *line)
 {
-  char *args[MAX];
+  	char *args[MAX]; // Set arguement array
   
-  char *backup = strdup(line);
-  
-  int arg_count = parseArg(line, args);
-  strcpy(line, backup);
-  free(backup);
-  
-  if(strcmp(command, "cd") == 0)
-    CD(args[0], arg_count);
-  else if(strcmp(command, "exit") == 0)
-  {
-    //Exit Command
-  }
-  else if(strcmp(command, "path") == 0)
+  	char *backup = strdup(line); // Create a backup for the command line
+  	
+	// Parse command line and free backup
+  	int arg_count = parseArg(line, args);
+  	strcpy(line, backup);
+  	free(backup);
+  	
+	// If built in instruction called, jump to fuction
+  	if(strcmp(command, "cd") == 0)
+  	  CD(args[0], arg_count);
+  	else if(strcmp(command, "exit") == 0)
+  	{
+  	  //Exit Command
+  	}
+  	else if(strcmp(command, "path") == 0)
 	{
-    //Path Command
-  }
-  else if(strcmp(command, "myhistory") == 0)
+	    //Path Command
+	}
+	else if(strcmp(command, "myhistory") == 0)	
 	{
-    //History Command
-  }
-  else if(strcmp(command, "alias") == 0)
+	    //History Command
+	}
+  	else if(strcmp(command, "alias") == 0)
 	{
-    //Alias Command
-  }
-  else comRedirect(args, command, arg_count, line);
+    	//Alias Command
+  	}
+  	else comRedirect(args, command, arg_count, line);
     
-    for(int i=0; i<arg_count-1; i++)
-    {
-      if(args[i] != NULL)
-      {
-        free(args[i]);
-        args[i] = NULL;
-      }
-    }
+	// Free memory
+    	for(int i=0; i<arg_count-1; i++)
+    	{
+      		if(args[i] != NULL)
+      		{
+        		free(args[i]);
+        		args[i] = NULL;
+      		}
+    	}
 }
