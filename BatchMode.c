20dcef243e3662b317c509bf4d93591f1b99f1d2
SammyT1178 <61430214+SammyT1178@users.noreply.github.com>
2022-12-07 08:50:33 -0600

    Update BatchMode.c
    Fixed readability

--- BatchMode.c
+++ BatchMode.c
@@ -2,36 +2,36 @@
 
 void BatchMode(char *file)
 {
-  // Open FILE
-  FILE *filepointer = fopen(file, "r");
-  if(filepointer == NULL)
-  {
-    printf("Error: File could not be opened or found\n");
-    MyExit();
-  }
+        // Open FILE
+        FILE *filepointer = fopen(file, "r");
+        if(filepointer == NULL)
+        {
+        printf("Error: File could not be opened or found\n");
+        MyExit();
+        }
   
-  // Reserve memory for the line of commands
-  char *command_line = (char *)malloc(MAX);
-  memset(command_line, '\0', sizeof(command_line));
+        // Reserve memory for the line of commands
+        char *command_line = (char *)malloc(MAX);
+        memset(command_line, '\0', sizeof(command_line));
   
-  //Read the line from the pointer and store it into command_line
-  while(fgets(command_line, MAX, filepointer))
-  {
-    command_line[strcspn(command_line, "\n")] = 0;  //Remove any newline
-    printf("Processing input: %s\n", command_line);
+        //Read the line from the pointer and store it into command_line
+        while(fgets(command_line, MAX, filepointer))
+        {
+                command_line[strcspn(command_line, "\n")] = 0;  //Remove any newline
+                printf("Processing input: %s\n", command_line);
     
-    // Send to parse function
-    int comNum = parseCom(command_line);
+                // Send to parse function
+                int comNum = parseCom(command_line);
     
-    // Execute each line
-    for(int n=0; n < comNum; n++)
-    {
-      char *temp = strdup(command[n]);
-      temp = strtok(temp, " ");
-      exeCom(temp, command[n]);
-      free(temp);  // Free temp
-    }
-  }
-  free(command_line); // Free command_line
-  fclose(filepointer); // Close FILE
+                // Execute each line
+                for(int n=0; n < comNum; n++)
+                {
+                        char *temp = strdup(command[n]);
+                        temp = strtok(temp, " ");
+                        exeCom(temp, command[n]);
+                        free(temp);  // Free temp
+                }
+        }
+        free(command_line); // Free command_line
+        fclose(filepointer); // Close FILE
 }
